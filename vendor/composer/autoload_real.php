2047d2c20977a80726e0e9fdf737f16c7cef1064
justlevine <justlevine@gmail.com>
2020-12-31 04:37:48 +0200

    Manually remove failed platform_check.php

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInit8e59707a5d3582830b47ec9f532e7191
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit8e59707a5d3582830b47ec9f532e7191', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit8e59707a5d3582830b47ec9f532e7191', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit8e59707a5d3582830b47ec9f532e7191::getInitializer($loader));
         } else {
